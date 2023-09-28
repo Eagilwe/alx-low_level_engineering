5b0f171b5d0257e37d37206bb4752a362a3f9459
Eagilwe <ntsimanyana8@gmail.com>
2023-09-28 16:36:20 -0700

    Task Advanced 01 Code

--- 0x14-bit_manipulation/100-get_endianness.c
+++ 0x14-bit_manipulation/100-get_endianness.c
@@ -5,9 +5,9 @@
  * Return: ) if bg endian, 1 if small endian.
  */
 
-int get_endianness(void)
+int get_endianness(void) 
 {
-unsigned int z = 1;
-unsigned char *x = (unsigned char *)&z;
-return (int)*z == 1 ? 1 : 0;
+unsigned int num = 1;
+unsigned char *byte = (unsigned char *)&num;
+return (int)*byte == 1 ? 1 : 0;
 }
